434bf13cb9dd42791b0c72361ca0a56bacd78711
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-08-23 16:01:23 +0530

    feat : design UI of home and integrate task functionality with UI and set the data in UI

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:task_todo/app/core/utils/extensions.dart';
+import 'package:task_todo/app/data/models/task.dart';
+import 'package:task_todo/app/modules/home/home_controller.dart';
+import 'package:task_todo/app/modules/home/widgets/add_card_view.dart';
+import 'package:task_todo/app/modules/home/widgets/task_card_view.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(4.0.wp),
+          children: [
+            Text(
+              'My List',
+              style: TextStyle(
+                fontSize: 24.0.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 3.0.hp,
+            ),
+            Obx(
+              () => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                mainAxisSpacing: 2.0.hp,
+                children: [
+                  ...controller.tasks
+                      .map((e) => LongPressDraggable(
+                          data: e,
+                          onDragStarted: () {
+                            controller.changeDeletingStatus(true);
+                          },
+                          onDraggableCanceled: (_, __) {
+                            controller.changeDeletingStatus(false);
+                          },
+                          onDragEnd: (_) {
+                            controller.changeDeletingStatus(false);
+                          },
+                          feedback: Opacity(
+                            opacity: 0.8,
+                            child: TaskCardView(task: e),
+                          ),
+                          child: TaskCardView(task: e)))
+                      .toList(),
+                  AddCardView(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: DragTarget<Task>(
+        builder: (_, __, ___) => Obx(
+          () => FloatingActionButton(
+            backgroundColor:
+                controller.deleting.value ? Colors.red : Colors.blue,
+            onPressed: () {},
+            child: Icon(
+              controller.deleting.value ? Icons.delete : Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        onAcceptWithDetails: (v) {
+          controller.deleteTask(v.data);
+          EasyLoading.showSuccess('Delete Success');
+        },
+      ),
+    );
+  }
+}
