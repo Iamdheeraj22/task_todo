ad325d3782239aaa39939300d37d5fb419efed86
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-08-27 16:06:06 +0530

    feat : implement right to swipe the todo item for delete

--- lib/app/modules/detail/widgets/done_todo_list_view.dart
+++ lib/app/modules/detail/widgets/done_todo_list_view.dart
@@ -8,56 +8,73 @@ class DoneTodoListView extends StatelessWidget {
   final ctrl = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ctrl.doneTodos.isEmpty
-          ? const Center(
-              child: Text('No one task is completed'),
-            )
-          : Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
-              child: ListView(
-                shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                children: [
-                  Text(
-                    'Completed(${ctrl.doneTodos.length})',
-                    style: TextStyle(
-                      fontSize: 14.0.sp,
-                      color: Colors.grey,
+    return Obx(
+      () => Container(
+        child: ctrl.doneTodos.isEmpty
+            ? const Center(
+                child: Text('No one task is completed'),
+              )
+            : Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  children: [
+                    Text(
+                      'Completed(${ctrl.doneTodos.length})',
+                      style: TextStyle(
+                        fontSize: 14.0.sp,
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 5.0,
-                  ),
-                  ...ctrl.doneTodos
-                      .map((e) => Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 3.0.wp, horizontal: 6.0.wp),
-                            child: Row(
-                              children: [
-                                const SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: Icon(Icons.done),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    ...ctrl.doneTodos
+                        .map((e) => Dismissible(
+                              direction: DismissDirection.endToStart,
+                              onDismissed: (v) {
+                                ctrl.removeDoneTask(e['title']);
+                              },
+                              background: Container(
+                                color: Colors.red.withOpacity(0.8),
+                                alignment: Alignment.centerRight,
+                                child: const Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
                                 ),
-                                SizedBox(
-                                  width: 3.0.wp,
+                              ),
+                              key: ObjectKey(e),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 3.0.wp, horizontal: 6.0.wp),
+                                child: Row(
+                                  children: [
+                                    const SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: Icon(Icons.done),
+                                    ),
+                                    SizedBox(
+                                      width: 3.0.wp,
+                                    ),
+                                    Text(
+                                      e['title'],
+                                      style: TextStyle(
+                                        fontSize: 14.0.sp,
+                                        decoration: TextDecoration.lineThrough,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  e['title'],
-                                  style: TextStyle(
-                                    fontSize: 14.0.sp,
-                                    decoration: TextDecoration.lineThrough,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ],
-                            ),
-                          ))
-                      .toList()
-                ],
+                              ),
+                            ))
+                        .toList()
+                  ],
+                ),
               ),
-            ),
+      ),
     );
   }
 }
