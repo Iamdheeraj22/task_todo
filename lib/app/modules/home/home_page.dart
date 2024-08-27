55144d2c758e1987f4187e992505fa2b456c5b56
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-08-27 22:36:19 +0530

    feat : design bottom-navigation for show different tabs(Home and Report details)

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -7,6 +7,7 @@ import 'package:task_todo/app/modules/home/home_controller.dart';
 import 'package:task_todo/app/modules/home/widgets/add_card_view.dart';
 import 'package:task_todo/app/modules/home/widgets/add_todo_dialog.dart';
 import 'package:task_todo/app/modules/home/widgets/task_card_view.dart';
+import 'package:task_todo/app/modules/report/report_details_page.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
@@ -14,55 +15,66 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.all(4.0.wp),
-          children: [
-            Text(
-              'My List',
-              style: TextStyle(
-                fontSize: 24.0.sp,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Obx(
+        () => IndexedStack(index: controller.tabIndex.value, children: [
+          SafeArea(
+            child: ListView(
+              padding: EdgeInsets.all(4.0.wp),
+              children: [
+                Text(
+                  'My List',
+                  style: TextStyle(
+                    fontSize: 24.0.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 3.0.hp,
+                ),
+                Obx(
+                  () => GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    mainAxisSpacing: 2.0.hp,
+                    children: [
+                      ...controller.tasks
+                          .map((e) => LongPressDraggable(
+                              data: e,
+                              onDragStarted: () {
+                                controller.changeDeletingStatus(true);
+                              },
+                              onDraggableCanceled: (_, __) {
+                                controller.changeDeletingStatus(false);
+                              },
+                              onDragEnd: (_) {
+                                controller.changeDeletingStatus(false);
+                              },
+                              feedback: Opacity(
+                                opacity: 0.8,
+                                child: TaskCardView(task: e),
+                              ),
+                              child: TaskCardView(task: e)))
+                          .toList(),
+                      AddCardView(),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 3.0.hp,
-            ),
-            Obx(
-              () => GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                mainAxisSpacing: 2.0.hp,
-                children: [
-                  ...controller.tasks
-                      .map((e) => LongPressDraggable(
-                          data: e,
-                          onDragStarted: () {
-                            controller.changeDeletingStatus(true);
-                          },
-                          onDraggableCanceled: (_, __) {
-                            controller.changeDeletingStatus(false);
-                          },
-                          onDragEnd: (_) {
-                            controller.changeDeletingStatus(false);
-                          },
-                          feedback: Opacity(
-                            opacity: 0.8,
-                            child: TaskCardView(task: e),
-                          ),
-                          child: TaskCardView(task: e)))
-                      .toList(),
-                  AddCardView(),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+          ReportDetailsPage()
+        ]),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: DragTarget<Task>(
         builder: (_, __, ___) => Obx(
           () => FloatingActionButton(
+            elevation: 5,
+            enableFeedback: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100.0),
+            ),
             backgroundColor:
                 controller.deleting.value ? Colors.red : Colors.blue,
             onPressed: () {
@@ -82,6 +94,27 @@ class HomePage extends GetView<HomeController> {
           controller.deleteTask(v.data);
           EasyLoading.showSuccess('Delete Success');
         },
+      ),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent),
+        child: Obx(
+          () => BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.tabIndex.value,
+            onTap: (v) {
+              controller.changeTabIndex(v);
+            },
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.data_usage), label: 'Report')
+            ],
+          ),
+        ),
       ),
     );
   }
